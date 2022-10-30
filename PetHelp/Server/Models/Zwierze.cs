e5235c9a0b06c4cc88aa02531ae41e3fcdc678b9
MrKomugiko <Mr.Komugiko@gmail.com>
2022-10-30 21:50:39 +0100

    podzielenie strony glownej na 2 czesci hodowle prawa zwierzaki

--- PetHelp/Server/Models/Zwierze.cs
+++ PetHelp/Server/Models/Zwierze.cs
@@ -6,23 +6,26 @@ public class Zwierze
 {
     [Key]
     public int Id { get; set; }
-    public string Imie{ get; set; } = "- nie podano -";
+    public string Imie { get; set; } = "- nie podano -";
     public string Gatunek { get; set; } = "- nie podano -";
     public string Rasa { get; set; } = "- nie podano -";
     public string Umaszczenie { get; set; } = "- nie podano -";
-    public DateTime DataUrodzenia { get; set; } = DateTime.Now;
-    public DateTime DataDodania { get; set; } = DateTime.Now;
-	public bool Kastracja { get; set; } = false;
-    public DateTime? Waga_Pomiar{ get; set; }
+    public DateTime DataUrodzenia { get; set; }
+    public DateTime DataDodania { get; set; }
+    public bool Kastracja { get; set; } = false;
+    public DateTime? Waga_Pomiar { get; set; }
     public float? Waga_Wartosc { get; set; }
-    public string Info_Dodatkowe{ get; set; } = String.Empty;
-    public string Info_Schorzenia{ get; set; } = String.Empty;
+    public string Info_Dodatkowe { get; set; } = String.Empty;
+    public string Info_Schorzenia { get; set; } = String.Empty;
     public string Info_Choroby { get; set; } = String.Empty;
     public bool Szczepienie_Wscieklizna_Status { get; set; } = false;
     public DateTime? Szczepienie_Wscieklizna_Data { get; set; }
     public DateTime? Szczepienie_Wscieklizna_NastepnyTermin { get; set; }
-    
-    public Zalacznik? Zdjecie { get; set; }
+
+
+    public Zalacznik? Zdjecie { get; set; } // TODO: zrobic liste
+    //public List<Zalacznik> Zalaczniki { get; set; } = new();
+
 
     [ForeignKey("Hodowla")]
     public int? HodowlaId { get; set; }
