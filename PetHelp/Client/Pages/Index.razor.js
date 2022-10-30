058dfdc56021be993d91f05141872f82829a8e1f
MrKomugiko <Mr.Komugiko@gmail.com>
2022-10-30 02:33:46 +0200

    dodanie skryptu js oraz jego podpięcie z kodem c#  (IJSObjectReference) do zamykania instancji okienka modala po sukcesie dodawania hodowli do bazy

---
+++ PetHelp/Client/Pages/Index.razor.js
@@ -0,0 +1,7 @@
+﻿function hideModal(element) {
+    var myModalEl = document.getElementById(element.id)
+    var myModal = bootstrap.Modal.getInstance(myModalEl)
+    myModal.hide()
+};
+
+export { hideModal };
