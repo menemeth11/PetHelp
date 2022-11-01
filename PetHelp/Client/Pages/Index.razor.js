73bfc58a0237aaa931d6c7634ee3b54a61f696ff
MrKomugiko <Mr.Komugiko@gmail.com>
2022-11-01 13:17:11 +0100

    drobna poprawa styli i dzialajaca animacja 'wjazdu' zwierzat przy scrolowaniu

--- PetHelp/Client/Pages/Index.razor.js
+++ PetHelp/Client/Pages/Index.razor.js
@@ -4,4 +4,20 @@
     myModal.hide()
 };
 
-export { hideModal };
+function checkBoxes(boxes) {
+    console.log('triggered checkBoxes');
+
+    const triggerBottom = window.innerHeight / 6 * 5;
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    })
+}
+
+export { hideModal, checkBoxes };
